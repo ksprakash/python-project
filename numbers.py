f31db9511d9979eb9a4b1147b2711562696d655d
ksprakash <ksaiprakash01@gmail.com>
2020-05-09 21:59:57 -0400

    Integers and Numbers Which I worked on

---
+++ numbers.py
@@ -0,0 +1,35 @@
+num1=5
+num2=10
+# All mathematical operators
+print(num1+num2)
+print(num1%num2)
+print(num1**num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+
+#Some functions we can use 
+print(abs(-5))
+
+print(round(3.75,1))
+print(pow(2,2))
+
+
+#All Assignment Operators
+print(num1   != num2)
+print(num1   == num2)
+print(num1   == num2)
+print(num1   <= num2)
+print( num1  >= num2)
+print(num1   >  num2)
+print(num1   <  num2)
+
+a='5'
+b='10'
+#this is parsing or type casting ;string literals changed into integers
+print(int(a) + int(b))
+
+
+numf=3.67
+print(type(numf))
